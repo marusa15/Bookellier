40999e1bf378f8660fe3221b584d3cf17d4a594a
Marusa Barle <marusa.barle@proteusnet.com>
2017-04-11 20:16:05 +0000

    Added the read more functionality.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var TasteKid_BASE_URL = 'https://www.tastekid.com/api/similar'; //maybe without similar?
+var retrievedObject = {};
 
+var TasteKid_BASE_URL = 'https://www.tastekid.com/api/similar'; 
 function getDataFromApi(searchTerm, callback) {
   var settings = {
     url: TasteKid_BASE_URL,
@@ -8,26 +9,30 @@ function getDataFromApi(searchTerm, callback) {
       type: 'books',
       info: 1,
       limit: 3,
-      k: '' 
+      k: '263777-Bookelli-ZCKH0EIS' 
     },
     dataType: 'jsonp', 
     type: 'GET',
     success: callback
+        
     
   //  TasteKid_BASE_URL, query, callback
   }
   $.ajax(settings);
 }
 
+// display functions
 
 function displayTasteKidSearchData(data) {
-  console.log(data);
+  retrievedObject = data; 
   var resultElement = '';
-  if (data.Similar.Results > 0) {
-    data.Similar.Results.forEach(function(item) {
-     resultElement += '<p>' + item.Name + ' <br><br> ' + item.wTeaser + ' <br><br>Wikipedia: ' + item.wUrl + '</p>';
+  if (data.Similar.Results) {
+    
+     data.Similar.Results.forEach(function(item) {
+     resultElement += '<h3 class="search-results">' + item.Name + '  <button class="more-info">+</button></h3><div class="description"></div>';
     });
   }
+  
   else {
     console.log("No results");
     resultElement += '<p>No results</p>';
@@ -35,6 +40,21 @@ function displayTasteKidSearchData(data) {
   
   $('.js-search-results').html(resultElement);
 }
+
+//when user clicks 'more', description displays
+
+var displayDescription = function (data, element) {
+    
+    var teaser = retrievedObject.Similar.Results.map(function(item) {
+    return '<p>' + item.wTeaser + '</p>';
+        
+    });
+
+    return element.html(teaser);
+}
+
+
+// event listeners
 
 function watchSubmit() {
   $('.js-search-form').submit(function(e) {
@@ -45,6 +65,22 @@ function watchSubmit() {
   });
 }
 
+//clicks on 'more' button
+
+$('.js-search-results').on('click', '.more-info', function(event) {
+  console.log("Hello more info!");
+  event.preventDefault();
+  displayDescription(retrievedObject, $('.description'));
+})
+
+
+
+
 $(function(){ 
   watchSubmit();
 });
+
+
+
+
+
