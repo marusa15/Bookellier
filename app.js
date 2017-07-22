cb2478dd6f3754b7383a45898249756ad7d0a64a
Marusa <marusa15@gmail.com>
2017-07-22 13:18:22 +0200

    Edited the styling of the description.

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ function displayTasteKidSearchData(data) {
 
 var displayDescription = function (state, element) {
   var teaser = currentItem.wTeaser;
-  var teaser = '<p>' + teaser + '<br> <p>More info: </p><a href="' + currentItem.wUrl + '">' + currentItem.wUrl + '</a>';
+  var teaser = '<h2>' + currentItem.Name + '</h2><br> <span><b>Description: </b></span>' + teaser + '<br> <p>More info: </p><a href="' + currentItem.wUrl + '">' + currentItem.wUrl + '</a>';
   return element.html(teaser); 
 }
 
